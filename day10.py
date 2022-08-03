e8b0ba53b94da6d6fd7853531781e46b557f3aa0
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-08-03 12:07:58 +0200

    Day 10 completed

--- day10.py
+++ day10.py
@@ -10,22 +10,85 @@ chunks = []
 with open("./data/day10.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
-        chunks.append(line.rstrip())
+        chunks.append([char for char in line.rstrip()])
 
-corrupted_chunks = []
-print(chunks)
+simplified_chunks = []
 
 for chunk in chunks:
-    chunk_dict = {"(": 0, ")": 0, "[": 0, "]": 0, "{": 0, "}": 0, "<": 0, ">": 0}
-    for char in chunk:
-        chunk_dict[char] += 1
-    print(chunk_dict)
-    if (
-        chunk_dict["("] < chunk_dict[")"]
-        or chunk_dict["["] < chunk_dict["]"]
-        or chunk_dict["{"] < chunk_dict["}"]
-        or chunk_dict["<"] < chunk_dict[">"]
-    ):
-        corrupted_chunks.append(chunk)
+    temp_chunk = chunk.copy()
+    index = 0
+    # We first remove consecutive openings and close brackets
+    while index < (len(temp_chunk) - 1):
+        is_parenth = temp_chunk[index] == "(" and temp_chunk[index + 1] == ")"
+        is_square = temp_chunk[index] == "[" and temp_chunk[index + 1] == "]"
+        is_curly = temp_chunk[index] == "{" and temp_chunk[index + 1] == "}"
+        is_angle = temp_chunk[index] == "<" and temp_chunk[index + 1] == ">"
+        if is_parenth or is_square or is_curly or is_angle:
+            # Pop elements by index is a NON commutative operation!
+            temp_chunk.pop(index + 1)
+            temp_chunk.pop(index)
+            index = index - 1
+        else:
+            index = index + 1
+    simplified_chunks.append(temp_chunk)
 
-print(corrupted_chunks)
+illegals_dict = {")": 0, "]": 0, "}": 0, ">": 0}
+for chunk in simplified_chunks:
+    for i in range(1, len(chunk), 1):
+        if chunk[i] == ")" and (
+            chunk[i - 1] == "[" or chunk[i - 1] == "{" or chunk[i - 1] == "<"
+        ):
+            illegals_dict[")"] += 1
+        elif chunk[i] == "]" and (
+            chunk[i - 1] == "(" or chunk[i - 1] == "{" or chunk[i - 1] == "<"
+        ):
+            illegals_dict["]"] += 1
+        elif chunk[i] == "}" and (
+            chunk[i - 1] == "(" or chunk[i - 1] == "[" or chunk[i - 1] == "<"
+        ):
+            illegals_dict["}"] += 1
+        elif chunk[i] == ">" and (
+            chunk[i - 1] == "(" or chunk[i - 1] == "[" or chunk[i - 1] == "{"
+        ):
+            illegals_dict[">"] += 1
+
+answer = (
+    illegals_dict[")"] * 3
+    + illegals_dict["]"] * 57
+    + illegals_dict["}"] * 1197
+    + illegals_dict[">"] * 25137
+)
+print(f"The total syntax error score is: {answer}")
+
+print(" ")
+print("----------------------------------------")
+print("For day 10 Part 2")
+
+legal_chunks = []
+for chunk in simplified_chunks:
+    if (
+        ")" not in chunk
+        and "]" not in chunk
+        and "}" not in chunk
+        and ">" not in chunk
+    ):
+        legal_chunks.append(chunk[::-1])  # we append in reverse order
+
+scores = []
+for chunk in legal_chunks:
+    score = 0
+    for element in chunk:
+        score *= 5
+        if element == "(":
+            score += 1
+        elif element == "[":
+            score += 2
+        elif element == "{":
+            score += 3
+        elif element == "<":
+            score += 4
+    scores.append(score)
+
+sorted_scores = sorted(scores)
+middle_score = sorted_scores[len(sorted_scores) // 2]
+print(f"The middle score is {middle_score}")
