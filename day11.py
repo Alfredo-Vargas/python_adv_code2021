c0207de0b9bc6afb7df55e6d5bbed7ad43cf68b7
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-08-04 17:22:15 +0200

    Day 11 completed - solved using recursion

--- day11.py
+++ day11.py
@@ -6,15 +6,17 @@ import numpy as np
 
 print(" ")
 print("----------------------------------------")
-print("For day 11 Part 1")
+print("For day 11 Part 1 and 2")
+global flash_counter
+flash_counter = 0
 
 # We add -infinity to the border lines so it will never flash as an octopus
 # Another matrix with the same dimension will keep track if they flashed
 with open("./data/day11.txt", "r") as file:
     lines = file.readlines()
-    octopuses_energy = np.ones((len(lines) + 2, len(lines[0].strip()) + 2))
-    temp_matrix = np.ones((len(lines) + 2, len(lines[0].strip()) + 2))
-    octopuses_flashed = temp_matrix != 1
+    nrows, ncols = len(lines) + 2, len(lines[0].rstrip()) + 2
+    octopuses_energy = np.ones((nrows, ncols))
+    octopuses_flashed = np.zeros((nrows, ncols), dtype=int)
     octopuses_energy[0, :] = -math.inf
     octopuses_energy[len(lines) + 1, :] = -math.inf
     octopuses_energy[:, 0] = -math.inf
@@ -24,37 +26,44 @@ with open("./data/day11.txt", "r") as file:
         octopuses_energy[i + 1, 1:-1] = np.array(temp_list)
 
 
-def adjacent_interaction():
-    for i in range(1, len(octopuses_energy[:, 0]) - 1, 1):
-        for j in range(1, len(octopuses_energy[0, :]) - 1, 1):
-            flashed = octopuses_flashed[i, j]
-            current_energy = octopuses_energy[i, j]
-            if not flashed and current_energy > 9:
-                octopuses_energy[i - 1][j - 1] += 1
-                octopuses_energy[i - 1][j] += 1
-                octopuses_energy[i - 1][j + 1] += 1
-                octopuses_energy[i][j - 1] += 1
-                octopuses_energy[i][j + 1] += 1
-                octopuses_energy[i + 1][j - 1] += 1
-                octopuses_energy[i + 1][j] += 1
-                octopuses_energy[i + 1][j + 1] += 1
-                octopuses_flashed[i, j] = True
+def adjacent_interaction(xpos, ypos):
+    global flash_counter
+    # Changed flashed to true immediately so inner recursion know about it
+    octopuses_flashed[xpos, ypos] = 1
+    flash_counter += 1
+    neighbour_points = [
+        (xpos - 1, ypos - 1),
+        (xpos - 1, ypos),
+        (xpos - 1, ypos + 1),
+        (xpos, ypos - 1),
+        (xpos, ypos + 1),
+        (xpos + 1, ypos - 1),
+        (xpos + 1, ypos),
+        (xpos + 1, ypos + 1),
+    ]
+
+    for point in neighbour_points:
+        if not octopuses_flashed[point[0], point[1]]:
+            octopuses_energy[point[0], point[1]] += 1
+            if octopuses_energy[point[0], point[1]] > 9:
+                adjacent_interaction(point[0], point[1])
 
 
-flash_counters = []
-for _ in range(2):
+for index in range(1000):
+    synched_number = np.count_nonzero(octopuses_energy == 0)
+    if synched_number == 100:
+        print(f"Synch happened at iteration: {index}")
+        break
+
     octopuses_energy += 1  # we start by adding one to every octopus
-    # get coordinates of the points with energy > 9
-    (x, y) = np.where((octopuses_energy > 9))
-    print(x, y)
-    # DO WHILE ENERGY VALUES ARE > 9 AND FLASHED IS FALSE
-    # iterate over those coordinates
-    # update the energy values and flash states
+    energetic_octopuses = np.argwhere(octopuses_energy > 9)
 
-    # adjacent_interaction()
-    # print(f"The point is: {i}, {j} with value {octopuses_energy[i, j]}")
-    # octopuses_energy[octopuses_energy > 9] = 0
-    # octopuses_flashed[octopuses_flashed == True] = False  # reset flash state
+    for i in range(len(energetic_octopuses)):
+        xpos = int(energetic_octopuses[i][0])
+        ypos = int(energetic_octopuses[i][1])
+        if octopuses_flashed[xpos, ypos] == 0:
+            adjacent_interaction(xpos, ypos)
+    octopuses_energy[octopuses_energy > 9] = 0
+    octopuses_flashed = octopuses_flashed * 0
 
-print(octopuses_energy)
-# All loops must run from 1, to 10 (borders are minus infinity)
+print(f"The number of flashes after {index} iterations is: {flash_counter}")
