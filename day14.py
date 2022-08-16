15ae2a2b6b511e2821ecd8fe16299344b157c66b
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-08-16 18:58:29 +0200

    Day 14 done, duplication rule instead of insertion
    The key to solve this problem was to change the insertion rule to
    duplication rule. For example the case where ABC with insertion rules
    looks as follows: AXBYC where we had two insertions for X and Y. Instead
    it is better to look as two initial pairs: AB - BC that then duplicate
    to the following pairs: AX XB BY YC, keep track of the duplication rules
    and count the number of ocurrences!.

--- day14.py
+++ day14.py
@@ -2,6 +2,7 @@
 
 # Day 14 part 1 and 2
 import sys
+import math
 from collections import defaultdict, Counter
 
 file = sys.argv[1] if len(sys.argv) > 1 else 'please input the 14 day data'
@@ -14,25 +15,57 @@ insertion_rules = defaultdict(str)
 template = file_content.readline().rstrip()
 file_content.readline()
 
+answer = defaultdict(int)
 for line in file_content:
     temp_key, temp_value = line.rstrip().split('->')
     key, value = temp_key.rstrip(), temp_value.lstrip()
     insertion_rules[key] = value
+    answer[value] = 0
 
+
+# INSIGHT:
+# Every pair of letters produce two pair of letters!!!
+# It is better to change the insertion rules to duplication rules
+
+duplication_rules = defaultdict(tuple)
+pair_counter = defaultdict(int)
+for rule in insertion_rules:
+    a = rule[0] + insertion_rules[rule]
+    b = insertion_rules[rule] + rule[1]
+    duplication_rules[rule] = (a, b)
+    pair_counter[rule] = 0
+
+# Initialize pair counter with given template
 polymer = list(template)
-N = 10  # number of iterations
-for _ in range(N):
-    temp_polymer = polymer.copy()
-    for i in range(len(polymer) - 1):
-        key = polymer[i] + polymer[i + 1]
-        value = insertion_rules[key]
-        temp_polymer.insert(2 * i + 1, value)
-    polymer = temp_polymer
-    print("".join(polymer))
+for i in range(len(polymer) - 1):
+    pair = polymer[i] + polymer[i + 1]
+    pair_counter[pair] += 1
 
-# frequency_counter = dict(Counter(polymer))
-#
-# max_value = max(frequency_counter.values())
-# min_value = min(frequency_counter.values())
-#
-# print(max_value - min_value)
+N = 40
+for i in range(N):
+    temp_counter = pair_counter.copy()
+    for pair in temp_counter:
+        if temp_counter[pair] != 0:
+            temp_value1 = duplication_rules[pair][0]
+            temp_value2 = duplication_rules[pair][1]
+            pair_counter[temp_value1] += temp_counter[pair]
+            pair_counter[temp_value2] += temp_counter[pair]
+            pair_counter[pair] -= temp_counter[pair]
+    # print(f"The number of pairs after {i+1}th iteration:")
+    # print(pair_counter)
+
+
+# We calculate the number of letters
+for letter in answer:
+    for pair in pair_counter:
+        if letter in pair:
+            if letter + letter != pair:
+                answer[letter] += pair_counter[pair]
+            else:
+                answer[letter] += 2 * pair_counter[pair]
+
+final_answer = list(answer.values())
+
+# A pair in the pair_counter doubles the correct value
+print(math.ceil(max(final_answer) / 2)  - math.ceil(min(final_answer) / 2))
+
