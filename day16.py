0c8fde3e8bafbae69c26a224612c90d5a194f72d
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-08-23 20:55:25 -0400

    2022-08-23 20:55:25

--- day16.py
+++ day16.py
@@ -23,39 +23,56 @@ hex_to_bin = {
     "F": "1111",
 }
 
-# bin_to_dec = {"100": 4, "110": 6, "001": 1}
-
 bin_packet = ""
 for char in packet:
     bin_packet += hex_to_bin[char]
-# print(bin_packet)
 
 def get_version_id(bin_packet):
     packet_version = int(bin_packet[0:3], 2)
     packet_id = int(bin_packet[3:6], 2)
     return (packet_version, packet_id)
 
+version_sum = 0
+cursor_position = 0
 
-print(f"The whole binary input is: \n {bin_packet}")
-packet_version, packet_id = get_version_id(bin_packet)
-print(f"The version and id are: {get_version_id(bin_packet)}")
+while True:
+    packet_version, packet_id = get_version_id(bin_packet)
+    print(f"The whole binary input is: {bin_packet}")
+    print(f"The version and id are: {get_version_id(bin_packet)}")
+    version_sum += packet_version
+    print(f"The version is: {packet_version}")
+    print(f"The version sum is: {version_sum}")
 
-if packet_id == 4:
-    groups = bin_packet[6:]
-    n_groups = len(bin_packet[6:]) // 5
+    if packet_id == 4:
+        bin_packet = bin_packet[6:]
+        packet_version, packet_id = get_version_id(bin_packet)
+        break
+    else:
+        length_type = int(bin_packet[6], 2)
+        if length_type == 0:
+            cursor_position = 22
+            bin_packet = bin_packet[cursor_position:]
+        else:
+            cursor_position = 18
+            bin_packet = bin_packet[cursor_position:]
 
-    literal_value = ""
-    for i in range(n_groups):
-        group = groups[5*i:(i+1)*5]
-        literal_value += group[1:]
-    numerical_value = int(literal_value, 2)    
-    print(f"It was a literal packet with value: {numerical_value}")
-else:
-    length_type_id = 15 if bin_packet[6] == "0" else 11
-    print(f"Length type id is: {length_type_id}")
-    subpackets_length = int(bin_packet[7:7+length_type_id], 2)
-    print(f"Subpackets length type is: {subpackets_length}")
-    if subpackets_length == 1:
-        print(bin_packet[6 + length_type_id:])
-        print(get_version_id(bin_packet[6 + length_type_id:]))
+print(version_sum)
 
+# if packet_id == 4:
+#     groups = bin_packet[6:]
+#     n_groups = len(bin_packet[6:]) // 5
+#
+#     literal_value = ""
+#     for i in range(n_groups):
+#         group = groups[5*i:(i+1)*5]
+#         literal_value += group[1:]
+#     numerical_value = int(literal_value, 2)    
+#     print(f"It was a literal packet with value: {numerical_value}")
+# else:
+#     length_type_id = 15 if bin_packet[6] == "0" else 11
+#     print(f"Length type id is: {length_type_id}")
+#     subpackets_length = int(bin_packet[7:7+length_type_id], 2)
+#     print(f"Subpackets length type is: {subpackets_length}")
+#     if subpackets_length == 1:
+#         print(bin_packet[6 + 1 + length_type_id:])
+#         print(get_version_id(bin_packet[6 + 1 + length_type_id:]))
