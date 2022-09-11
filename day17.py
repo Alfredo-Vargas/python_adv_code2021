0f6b94a6097b782ad47cd82d1be8032eb712c46b
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-09-11 13:05:46 +0200

    day 17 optimized

--- day17.py
+++ day17.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 infile = sys.argv[1] if len(sys.argv) > 1 else "17.in"
 xraw, yraw = open(infile).read().strip()[13:].split(",")
@@ -7,16 +8,20 @@ pymin, pymax = yraw.strip()[2:].split("..")
 xmin, xmax = int(pxmin), int(pxmax)
 ymin, ymax = int(pymin), int(pymax)
 
-#  we guess a reasonable time interval where that should happen
-t_range = [i for i in range(1, 2000)]
 
-# we guess a reasonable vx0 velocity to be evaluated
-vx0_range = [vx0 for vx0 in range(0, 2000)]
-# we guess a reasonable vy0 velocity to be evaluated
-vy0_range = [vy0 for vy0 in range(-2000, 2000)]
+# v0xmin is given by the "n" consecutive numbers n(n+1)/2 = xmin
+v0xmin = int((-1 + math.sqrt(1 + 2 * xmin)) / 2)
+# vx0 = xmax + 1 misses the target after one time step therefore is the max
+vx0_range = [vx0 for vx0 in range(v0xmin, xmax + 1)]
 
-initial_x = []
-initial_y = []
+# vy0 = ymin - 1 misses the target after one time step therefore is the min
+# vy0 = -ymin + 1 misses the target after one time step therefore is the max
+vy0_range = [vy0 for vy0 in range(ymin - 1, -ymin + 1)]
+
+# We guess a reasonable high time interval (not problem dependent ... sigh)
+trange = [i for i in range(1, 1000)]
+
+xrange, yrange = [], []
 
 
 def hit_target(pos, r):
@@ -27,65 +32,50 @@ def hit_target(pos, r):
         if pos >= ymin and pos <= ymax:
             return True
     else:
-        print("Direction not specified. Please give either \"x\" or \"y\".")
+        print(f"Direction unknown: {r}. Please give either \"x\" or \"y\".")
     return False
 
 
+# xmotion we save points (time, vx0) that hit the target
 for vx0 in vx0_range:
     xpos, v0 = 0, vx0
-    for t in t_range:
+    for t in trange:
         xpos += v0
         v0 = v0 - 1 if v0 > 0 else 0
         if hit_target(xpos, "x"):
-            initial_x.append((t, vx0))
+            xrange.append((t, vx0))
 
-
+# ymotion we save points (time, vy0) that hit the target
 for vy0 in vy0_range:
     ypos, v0 = 0, vy0
-    for t in t_range:
+    for t in trange:
         ypos += v0
         v0 = v0 - 1
         if hit_target(ypos, "y"):
-            initial_y.append((t, vy0))
+            yrange.append((t, vy0))
 
-selected = []
-for yvalues in initial_y:
-    for xvalues in initial_x:
+# velocities v0x and v0y that hit the target at the same time
+v0x, v0y = [], []
+for yvalues in yrange:
+    for xvalues in xrange:
         if yvalues[0] == xvalues[0]:
-            selected.append((xvalues[1], yvalues[1]))
+            v0x.append(xvalues[1])
+            v0y.append(yvalues[1])
 
-vy0 = 0
-for v in selected:
-    if v[1] > vy0:
-        vy0 = v[1]
-
-filtered = []
-for v in selected:
-    if v[1] == vy0:
-        filtered.append(v)
-
-
-vx0 = 9999
-for v in filtered:
-    if v[0] < vx0:
-        vx0 = v[0]
-
-v0 = []
-for v in filtered:
-    if v[0] == vx0:
-        v0.append(v)
+v0ymax = max(v0y)
 
 # We calculate the maximum height
-y0, vz = 0, v0[0][1]
-for t in t_range:
-    y0 += vz
-    vz = vz - 1
-    if vz == 0:
-        print(f"The maximum height is: {y0}.\n")
+y, vy = 0, v0ymax
+for t in trange:
+    y += vy
+    vy = vy - 1
+    if vy == 0:
+        print("\nPart 1, solution:")
+        print(f"The maximum height is: {y}.")
         break
 
-set_selected = set(selected)
-print(len(set_selected))
 
-# Part 1 answer: 12090
-# Part 2 answer: 5059
+# The total velocities pairs that hit target (included repeated)
+v0_pairs = list(zip(v0x, v0y))
+print("\npart 2, solution:")
+print(f"Number of velocities which hit the target: {len(set(v0_pairs))}.")
