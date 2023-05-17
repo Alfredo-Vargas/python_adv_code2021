7d737eeecb43f9fc84e7a8bc2b59f8218f0cd936
Your Name <you@example.com>
2023-05-18 00:14:30 +0200

    Day20 pt2 taken fron the amazing Jonhatan Paulson

--- day20.py
+++ day20.py
@@ -5,7 +5,6 @@ import copy
 # reading and parsing input
 INPUT_TXT = os.path.join(os.path.dirname(__file__), "./data/day20.txt")
 INPUT_S = os.path.join(os.path.dirname(__file__), "./data/test.txt")
-EXPECTED = 35
 
 
 def read_input(INPUT_TXT: str, padding: int) -> tuple:
@@ -28,7 +27,10 @@ def read_input(INPUT_TXT: str, padding: int) -> tuple:
             input_image[-1 - i] += vertical_padding
     return (input_image, algorithm)
 
-def apply_filter(input_image: list, algorithm: str, output_image: list, padding: int) -> list:
+
+def apply_filter(
+    input_image: list, algorithm: str, output_image: list, padding: int
+) -> list:
     output_image = copy.deepcopy(input_image)
     n_rows = len(input_image)
     n_cols = len(input_image[0])
@@ -54,57 +56,92 @@ def apply_filter(input_image: list, algorithm: str, output_image: list, padding:
 def count_lit_pixels(input_image: list) -> int:
     lit_counter = 0
     for row in input_image:
-        lit_counter += row.count('#')
+        lit_counter += row.count("#")
     return lit_counter
 
-def compute(input: str) -> int:
-    print_images = False
-    padding = 3  # will depend on the number of the interations
+
+def compute(input: str, n_iter: int) -> int:
+    # print_images = False
+    padding = n_iter + 1  # will depend on the number of the interations
     input_image, algorithm = read_input(input, padding)
-    if print_images:
-        print(f"The algorithm is:\n{algorithm}")
+    output_image = list()
+
+    for i in range(n_iter):
+        # output_image = copy.deepcopy(input_image)
+        input_image = apply_filter(input_image, algorithm, output_image, padding)
+        padding -= 1
+        if i == 48:
+            print(count_lit_pixels(input_image))
+        # input_image = output_image
+
+    # if print_images:
+    #     print(f"The algorithm is:\n{algorithm}")
+
+    # if print_images:
+    #     print(f"The input image is:\n")
+    #     for line in input_image:
+    #         print(line)
+
+    # output1 = copy.deepcopy(input_image)
+    # output1 = apply_filter(input_image, algorithm, output1, padding)
+
+    # padding -= 1
+
+    # if print_images:
+    #     print("\nAfter applying the filter one time:")
+    #     for line in output1:
+    #         print(line)
+
+    # output2 = copy.deepcopy(output1)
+    # output2 = apply_filter(output1, algorithm, output2, padding)
+    # padding -= 1
+
+    # if print_images:
+    #     print("\nAfter applying the filter the second time:")
+    #     for line in output2:
+    #         print(line)
+
+    # if print_images:
+    #     print(f"\nThe lit counter is: {count_lit_pixels(output2)}")
+    return count_lit_pixels(input_image)
 
 
-    if print_images:
-        print(f"The input image is:\n")
-        for line in input_image:
-            print(line)
+# INPUT_S = """\
+# ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
 
-    output1 = copy.deepcopy(input_image)
-    output1 = apply_filter(input_image, algorithm, output1, padding)
+# #..#.
+# #....
+# ##..#
+# ..#..
+# ..###
+# """
+EXPECTED = 35
 
-    padding -= 1
-
-    if print_images:
-        print("\nAfter applying the filter one time:")
-        for line in output1:
-            print(line)
-
-    output2 = copy.deepcopy(output1)
-    output2 = apply_filter(output1, algorithm, output2, padding)
-    padding -= 1
-
-    if print_images:
-        print("\nAfter applying the filter the second time:")
-        for line in output2:
-            print(line)
-
-    if print_images:
-        print(f"\nThe lit counter is: {count_lit_pixels(output2)}")
-    return count_lit_pixels(output2)
 
 @pytest.mark.parametrize(
-        ("input_s", "expected"),
-        ((INPUT_S, EXPECTED),),
-        )
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
 def test(input_s: str, expected: int) -> None:
-    assert compute(input_s) == expected
+    assert compute(input_s, 2) == expected
 
 
 def main() -> int:
-    print(f"Part 1 solution is: {compute(INPUT_TXT)}")
+    are_we_testing = True
+    if are_we_testing:
+        print(f"Part 1 solution is: {compute(INPUT_S, 2)}")
+        # print(f"Part 2 solution is: {compute(INPUT_S, 50)}")
+    else:
+        print(f"Part 1 solution is: {compute(INPUT_TXT, 2)}")
+        # print(f"Part 2 solution is: {compute(INPUT_TXT, 50)}")
 
     return 0
+
+# right answer part1: 5583
+
+# failed part 2
+# 21024 (too high)
+# 20911 (too high)
 
 
 if __name__ == "__main__":
